feb27fe77fd63defad04ef308f5783e89506832f
hfkghgk <mohamedadelmobark2020@gmail.com>
2025-03-20 16:28:23 -0700

    done

--- session_4/task9.dart
+++ session_4/task9.dart
@@ -1,11 +1,4 @@
-class Rectangle {
-  double _width;
-  double _height;
-
-  Rectangle(this._width, this._height);
-
-  double get area => _width * _height;
-}
+import 'classRectangle.dart';
 
 void main() {
   Rectangle rect = Rectangle(5.0, 10.0);
